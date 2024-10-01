45940421d9f7938bc433a58982132aee418ffcb2
Mora <qiuzhongleiabc@126.com>
2024-10-01 21:50:26 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # and-or
 
-支持 & 和 | 组合的字符串
+支持 & 和 | 组合的字符串，方便以字符串的形式配置逻辑组合条件
 
 
 ## Installation
 
+```bash
+npm i -S and-or
+```
+
 
 ## Usage
+
+```js
+import { andOr } from 'and-or'
+
+// 指定字符串中包含元音字母就返回 true
+const isContainsVowel = (str) => [ 'a', 'e', 'i', 'o', 'u' ].some(k => str.includes(k))
+
+andOr('a && (b || c)', isContainsVowel) // 等介于执行 `true && (false || false)`，所以最终返回 false
+```
 
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export * from './foo/index.js'
+const pattern = /[^|&()]+/g
+
+/**
+ * 解析字符串 "a && (b || c)"，并返回最终的结果
+ * @param template 模板字符串，支持 `&&` 或 `&`，表示逻辑与；也支持 `||` 或 `|`，表示逻辑或；默认逻辑与的优先级比逻辑或高，但可以使用 `()` 调整
+ * @param is
+ */
+export function andOr(template: string, is: (key: string) => boolean): boolean {
+  // 生成一个只包含 true 和 false 的表达式，如 "true & (true | false)"
+  const expression = template.replace(pattern, (r) => {
+    const key = r.trim()
+    if (!key) return '' // 有可能是在两个标签之间，如 `( a || b ) && c` 中 ")" 和 "&&" 之间也会匹配成功
+    return `${!!is(key)}`
+  })
+  try {
+    /* eslint-disable-next-line */
+    return eval(expression)
+  } catch (e) {
+    throw new Error(`Unexpected transform "${template}" => "${expression}"`)
+  }
+}
